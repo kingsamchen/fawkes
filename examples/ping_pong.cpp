18b5fc5426364e2218dff467329be8827df99396
Kingsley Chen <kingsamchen@gmail.com>
2025-07-27 01:23:01 +0800

    Add github CI

--- examples/ping_pong.cpp
+++ examples/ping_pong.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2025 - present, Kingsley Chen. All rights reserved.
+// This file is subject to the terms of license that can be found
+// in the LICENSE file.
 
 #include <boost/asio/io_context.hpp>
 #include <gflags/gflags.h>
@@ -8,9 +11,8 @@
 DEFINE_uint32(port, 9876, "Port number to listen");
 
 int main() {
-    boost::asio::io_context io_ctx{1};
-
     try {
+        boost::asio::io_context io_ctx{1};
         fawkes::server svc(io_ctx);
         svc.listen_and_serve("0.0.0.0", static_cast<std::uint16_t>(FLAGS_port));
         io_ctx.run();
