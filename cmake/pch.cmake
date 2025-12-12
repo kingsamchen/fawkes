c0645e65aeac4fef14c2d614b24d67ccae0342ff
Kingsley Chen <kingsamchen@gmail.com>
2025-12-12 17:09:54 +0800

    Change target names convention and use alias for pch
    Force using CMake 4.2.x on Windows solves the issue that MSVC tries to
    create a folder whose name would contain `:` which happens to be an
    illegal character of Windows paths

--- cmake/pch.cmake
+++ cmake/pch.cmake
@@ -29,18 +29,19 @@ set(FAWKES_PCH_HEADERS_BASE
   "<vector>"
 )
 
-add_library(fawkes.pch OBJECT)
+add_library(fawkes_pch OBJECT)
+add_library(fawkes::pch ALIAS fawkes_pch)
 
-target_sources(fawkes.pch
+target_sources(fawkes_pch
   PRIVATE
     ${FAWKES_DIR}/build/pch/pch.cpp
 )
 
-fawkes_common_compile_configs(fawkes.pch)
+fawkes_common_compile_configs(fawkes_pch)
 
-fawkes_use_sanitizers(fawkes.pch)
+fawkes_use_sanitizers(fawkes_pch)
 
-target_precompile_headers(fawkes.pch
+target_precompile_headers(fawkes_pch
   PRIVATE
     "${FAWKES_PCH_HEADERS_BASE}"
 )
